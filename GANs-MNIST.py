9c76cd84eabf7d4e9f1d5f20901175d6f2d48a39
DanielLongo <danieldominiclongo@gmail.com>
2018-11-24 21:03:45 -0800

    CapsGAN working

--- GANs-MNIST.py
+++ GANs-MNIST.py
@@ -2,10 +2,18 @@ from gan_utils import train_gan
 from utils import get_mnist_data
 from ConvDiscriminator import ConvDiscriminatorMNIST
 from ConvGenerator import ConvGeneratorMNIST
+from CapsDiscriminator import CapsDiscriminatorMNIST
 
+train_data, test_data = get_mnist_data()
+
+#DCGAN -> G: Conv D: Conv
 G = ConvGeneratorMNIST()
 D = ConvDiscriminatorMNIST()
 
-train_data, test_data = get_mnist_data()
+# train_gan(G,D, train_data, 10, 64, save_gen_images=True)
+
+#CapsGAN -> G: Conv D: Caps
+G = ConvGeneratorMNIST()
+D = CapsDiscriminatorMNIST() 
 
 train_gan(G,D, train_data, 10, 64, save_gen_images=True)
